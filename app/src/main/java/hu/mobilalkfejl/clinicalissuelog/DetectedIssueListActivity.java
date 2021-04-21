dbc7a851f059b1b35dade7e09724e5051badea27
Horváth Noel <h983268@stud.u-szeged.hu>
2021-04-22 00:42:06 +0200

    Remove unnecessary imports and correct typo

--- app/src/main/java/hu/mobilalkfejl/clinicalissuelog/DetectedIssueListActivity.java
+++ app/src/main/java/hu/mobilalkfejl/clinicalissuelog/DetectedIssueListActivity.java
@@ -1,10 +1,7 @@
 package hu.mobilalkfejl.clinicalissuelog;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,25 +13,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
 public class DetectedIssueListActivity extends AppCompatActivity {
     private static final String LOG_TAG = DetectedIssueListActivity.class.getName();
 
-    private static String currentPractitonerEmail;
+    private static String currentPractitionerEmail;
 
 
     private RecyclerView recyclerView;
@@ -52,7 +43,7 @@ public class DetectedIssueListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detected_issue_list);
 
-        currentPractitonerEmail = this.getIntent().getExtras().get("currentPractitionerEmail").toString();
+        currentPractitionerEmail = this.getIntent().getExtras().get("currentPractitionerEmail").toString();
 
         detectedIssueList = new ArrayList<>();
 
@@ -114,7 +105,7 @@ public class DetectedIssueListActivity extends AppCompatActivity {
 
             case R.id.createDetectedIssue:
                 Intent intent = new Intent(this,CreateDetectedIssueActivity.class);
-                intent.putExtra("currentPractitionerEmail",currentPractitonerEmail);
+                intent.putExtra("currentPractitionerEmail",currentPractitionerEmail);
                 startActivity(intent);
                 return true;
 
