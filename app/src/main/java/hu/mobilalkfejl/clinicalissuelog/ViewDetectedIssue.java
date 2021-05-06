a000ff55eababe123edb14d39e603d3e845e0fcc
noelhorvath <hnoel2000@gmail.com>
2021-05-06 10:10:45 +0200

    Create ViewDetectedIssueActivity and layout for activity

--- app/src/main/java/hu/mobilalkfejl/clinicalissuelog/ViewDetectedIssue.java
+++ app/src/main/java/hu/mobilalkfejl/clinicalissuelog/ViewDetectedIssue.java
@@ -3,6 +3,7 @@ package hu.mobilalkfejl.clinicalissuelog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewDetectedIssue extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class ViewDetectedIssue extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_detected_issue);
+
+
+    }
+
+    public void editDetectedIssue(View view) {
+    }
+
+    public void deleteDetectedIssue(View view) {
     }
 }
