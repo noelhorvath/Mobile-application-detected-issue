1822cdb51f4361cd2026b4aca055cdd96c080f79
noelhorvath <hnoel2000@gmail.com>
2021-05-08 12:41:20 +0200

    add onResume for reloading DetectedIssue info after updating , set correct offset for datetime and create implement onActivityForResult for Editing activity

--- app/src/main/java/hu/mobilalkfejl/clinicalissuelog/ViewDetectedIssueActivity.java
+++ app/src/main/java/hu/mobilalkfejl/clinicalissuelog/ViewDetectedIssueActivity.java
@@ -1,7 +1,10 @@
 package hu.mobilalkfejl.clinicalissuelog;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -15,10 +18,15 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class ViewDetectedIssueActivity extends AppCompatActivity {
 
     private FirebaseFirestore firestore;
     private String detectedIssueId;
+    private boolean reloadNeeded;
 
     TextView viewDetectedIssuePatientTW;
     TextView viewDetectedIssueCodeTW;
@@ -26,6 +34,8 @@ public class ViewDetectedIssueActivity extends AppCompatActivity {
     TextView viewDetectedIssueDetailTW;
     TextView viewDetectedIssueSeverityTW;
     TextView viewDetectedIssueIdentifiedDateTimeTW;
+
+    private final int INTENT_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +67,8 @@ public class ViewDetectedIssueActivity extends AppCompatActivity {
                 viewDetectedIssueStatusTW.setText(detectedIssue.getStatus());
                 viewDetectedIssueDetailTW.setText(detectedIssue.getDetail());
                 viewDetectedIssueSeverityTW.setText(detectedIssue.getSeverity());
-                viewDetectedIssueIdentifiedDateTimeTW.setText(detectedIssue.getIdentifiedDateTime().toDate().toInstant().toString().replace("Z"," ").replace("T", " "));
+                Instant instant = detectedIssue.getIdentifiedDateTime().toDate().toInstant();
+                viewDetectedIssueIdentifiedDateTimeTW.setText(LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toString().replace("T"," "));
 
             }
         });
@@ -70,12 +81,36 @@ public class ViewDetectedIssueActivity extends AppCompatActivity {
     }
 
     public void editDetectedIssue(View view) {
+        Intent intent = new Intent(this,EditDetectedIssueActivity.class);
+        intent.putExtra("detectedIssueId",detectedIssueId);
+        this.startActivityForResult(intent,INTENT_CODE);
     }
 
     public void deleteDetectedIssue(View view) {
         new DeleteDetectedIssueAsyncTask().execute(detectedIssueId);
         Toast.makeText(this,"Detected issue has been successfully deleted!",Toast.LENGTH_LONG).show();
+        Intent returnIntent = new Intent();
+        setResult(Activity.RESULT_OK,returnIntent);
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2) {
+            if(resultCode == Activity.RESULT_OK){
+                reloadNeeded = true;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(reloadNeeded){
+            reloadNeeded = false;
+            initText(detectedIssueId);
+        }
     }
 
     public void goBackToListActivity(View view) {
